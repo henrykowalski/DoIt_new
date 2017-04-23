decb4abd1e8dfc8b02893bd52b1f89e59b730997
Andrzej Palec <andrzej@Andrzejs-MacBook-Pro.local>
2017-04-23 17:36:16 +0200

    3x tasks manually

---
+++ DoIt_new/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt_new
+//
+//  Created by Andrzej Palec on 23/04/2017.
+//  Copyright © 2017 Andrzej Palec. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false    
+}

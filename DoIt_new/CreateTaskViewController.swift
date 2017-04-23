f96a7efad01d6dc52998d6c1eac434e166048b36
Andrzej Palec <andrzej@Andrzejs-MacBook-Pro.local>
2017-04-23 22:57:04 +0200

    Lze pridat novy Task a priradit mu dulezitost a pak se i zaradi do seznamu (array) a jeste se automaticky po ulozeni nacte uvodni stranka… vse nakonec funguje jak ma…

---
+++ DoIt_new/CreateTaskViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt_new
+//
+//  Created by Andrzej Palec on 23/04/2017.
+//  Copyright © 2017 Andrzej Palec. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        
+        // Create a Task from the outlet information
+        
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        // Add new task to array in previous viewController
+        
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+        
+    }
+  
+
+  
+
+}
